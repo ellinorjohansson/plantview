9df4a0baa3abc692bfa856de74186db3e176e0a9
ellinorjohansson <ellinor.asaka@gmail.com>
2026-07-19 12:54:13 +0200

    💄(style): changed font sizes and w, h

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import SvgPlant from "./src/components/SvgPlant";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center px-6">
+    <div className="flex min-h-screen">
       <main className="card-soft w-full max-w-4xl p-10 md:p-16">
         <section>
           <div className="flex flex-row gap-5">
             <span
-              className="bg-white/10 backdrop-blur-md border border-white/20 shadow-lg rounded-full px-3 py-3 w-19 h-19"
+              className="bg-yellow-200/10 backdrop-blur-md border border-white/20 shadow-lg rounded-full px-3 py-3 w-15 h-15"
               style={{
                 animation: "float 3s ease-in-out infinite",
               }}
@@ -15,7 +15,7 @@ export default function Home() {
               <SvgPlant />
             </span>
             <div>
-              <h1 className="font-serif text-5xl font-semibold flex items-center mb-1">
+              <h1 className="font-serif text-4xl font-semibold flex items-center mb-1">
                 Sprout Diary
               </h1>
               <p className="text-black/50">
