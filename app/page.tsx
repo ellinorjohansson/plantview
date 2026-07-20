a922be08abcd4d65e56df5464c9c9302b57b31e1
ellinorjohansson <ellinor.asaka@gmail.com>
2026-07-20 11:37:44 +0200

    💄(style): added overview section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Footer from "./src/sections/Footer";
 import HeaderSection from "./src/sections/HeaderSection";
+import OverviewSection from "./src/sections/OverviewSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <div className="mx-auto flex w-full max-w-4xl flex-col px-4 py-6 sm:px-6 sm:py-8 md:px-8 md:py-10">
           <HeaderSection />
         </div>
+        <OverviewSection />
       </main>
       <footer className="w-full px-4 pb-4 sm:px-6 sm:pb-6 md:px-8 md:pb-8">
         <div className="mx-auto flex w-full max-w-4xl justify-center">
---
+++ app/src/sections/OverviewSection.tsx
@@ -0,0 +1,101 @@
+const OverviewSection = () => {
+  return (
+    <section className="flex flex-row gap-5 px-4 md:px-8 lg:px-12">
+      <div className="bg-amber-50 rounded-4xl p-5 w-full backdrop-blur-md border border-white/10 shadow-lg">
+        <div className="flex flex-row justify-between items-center">
+          <span className="text-black/60 text-xs font-sans">
+            IN YOUR GARDEN
+          </span>
+          <div className="bg-green-700/20 rounded-full p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-leaf"
+              viewBox="0 0 16 16"
+            >
+              <path d="M1.4 1.7c.216.289.65.84 1.725 1.274 1.093.44 2.884.774 5.834.528l.37-.023c1.823-.06 3.117.598 3.956 1.579C14.16 6.082 14.5 7.41 14.5 8.5c0 .58-.032 1.285-.229 1.997q.198.248.382.54c.756 1.2 1.19 2.563 1.348 3.966a1 1 0 0 1-1.98.198c-.13-.97-.397-1.913-.868-2.77C12.173 13.386 10.565 14 8 14c-1.854 0-3.32-.544-4.45-1.435-1.125-.887-1.89-2.095-2.391-3.383C.16 6.62.16 3.646.509 1.902L.73.806zm-.05 1.39c-.146 1.609-.008 3.809.74 5.728.457 1.17 1.13 2.213 2.079 2.961.942.744 2.185 1.22 3.83 1.221 2.588 0 3.91-.66 4.609-1.445-1.789-2.46-4.121-1.213-6.342-2.68-.74-.488-1.735-1.323-1.844-2.308-.023-.214.237-.274.38-.112 1.4 1.6 3.573 1.757 5.59 2.045 1.227.215 2.21.526 3.033 1.158.058-.39.075-.782.075-1.158 0-.91-.288-1.988-.975-2.792-.626-.732-1.622-1.281-3.167-1.229l-.316.02c-3.05.253-5.01-.08-6.291-.598a5.3 5.3 0 0 1-1.4-.811" />
+            </svg>
+          </div>
+        </div>
+        <span className="text-4xl font-fraunces! font-bold!">0</span>
+      </div>
+
+      <div className="bg-amber-50 rounded-4xl p-5 w-full backdrop-blur-md border border-white/10 shadow-lg">
+        <div className="flex flex-row justify-between items-center">
+          <span className="text-black/60 text-xs font-sans">THIRSTY TODAY</span>
+          <div className="bg-green-700/20 rounded-full p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-droplet"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.21.8C7.69.295 8 0 8 0q.164.544.371 1.038c.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8m.413 1.021A31 31 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87z"
+              />
+            </svg>
+          </div>
+        </div>
+        <span className="text-4xl font-fraunces! font-bold!">0</span>
+      </div>
+
+      <div className="bg-amber-50 rounded-4xl p-5 w-full backdrop-blur-md border border-white/10 shadow-lg">
+        <div className="flex flex-row justify-between items-center">
+          <span className="text-black/60 text-xs font-sans">
+            WATERINGS LOGGED
+          </span>
+          <div className="bg-green-700/20 rounded-full p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-droplet"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.21.8C7.69.295 8 0 8 0q.164.544.371 1.038c.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8m.413 1.021A31 31 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87z"
+              />
+            </svg>
+          </div>
+        </div>
+        <span className="text-4xl font-fraunces! font-bold!">0</span>
+      </div>
+
+      <div className="bg-amber-50 rounded-4xl p-5 w-full backdrop-blur-md border border-white/10 shadow-lg">
+        <div className="flex flex-row justify-between items-center">
+          <span className="text-black/60 text-xs font-sans">NEWLY PLANTED</span>
+          <div className="bg-green-700/20 rounded-full p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-leaf"
+              viewBox="0 0 16 16"
+            >
+              <path d="M1.4 1.7c.216.289.65.84 1.725 1.274 1.093.44 2.884.774 5.834.528l.37-.023c1.823-.06 3.117.598 3.956 1.579C14.16 6.082 14.5 7.41 14.5 8.5c0 .58-.032 1.285-.229 1.997q.198.248.382.54c.756 1.2 1.19 2.563 1.348 3.966a1 1 0 0 1-1.98.198c-.13-.97-.397-1.913-.868-2.77C12.173 13.386 10.565 14 8 14c-1.854 0-3.32-.544-4.45-1.435-1.125-.887-1.89-2.095-2.391-3.383C.16 6.62.16 3.646.509 1.902L.73.806zm-.05 1.39c-.146 1.609-.008 3.809.74 5.728.457 1.17 1.13 2.213 2.079 2.961.942.744 2.185 1.22 3.83 1.221 2.588 0 3.91-.66 4.609-1.445-1.789-2.46-4.121-1.213-6.342-2.68-.74-.488-1.735-1.323-1.844-2.308-.023-.214.237-.274.38-.112 1.4 1.6 3.573 1.757 5.59 2.045 1.227.215 2.21.526 3.033 1.158.058-.39.075-.782.075-1.158 0-.91-.288-1.988-.975-2.792-.626-.732-1.622-1.281-3.167-1.229l-.316.02c-3.05.253-5.01-.08-6.291-.598a5.3 5.3 0 0 1-1.4-.811" />
+            </svg>
+          </div>
+        </div>
+        <span className="text-4xl font-fraunces! font-bold!">0</span>
+      </div>
+    </section>
+  );
+};
+
+export default OverviewSection;
